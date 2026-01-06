7fca11355eee1e63dc8a6e014e8fccbe27c72c74
Hugh Bellamy <hughbellars@gmail.com>
2026-01-06 19:07:33 +0000

    Minor cleanups

--- src/HFSForkType.cs
+++ src/HFSForkType.cs
@@ -3,15 +3,15 @@ namespace HfsReader;
 /// <summary>
 /// Represents the fork type for an extent key.
 /// </summary>
-public enum HFSForkType : byte
+public enum HFSForkType
 {
     /// <summary>
     /// Data fork.
     /// </summary>
-    DataFork = 0x00,
+    DataFork,
 
     /// <summary>
     /// Resource fork.
     /// </summary>
-    ResourceFork = 0xFF
+    ResourceFork
 }
