a6e43086238ad42d1b8f522ae47cccb9cbf3a435
Hugh Bellamy <hughbellars@gmail.com>
2026-01-31 18:11:50 +0000

    cleanup

--- src/HFSDisk.cs
+++ src/HFSDisk.cs
@@ -52,7 +52,7 @@ public class HFSDisk
                 throw new InvalidDataException("Unable to read Apple Partition Map Entry.");
             }
 
-            var partitionEntry = new ApplePartitionMapEntry(blockBuffer.Slice(0, ApplePartitionMapEntry.Size));
+            var partitionEntry = new ApplePartitionMapEntry(blockBuffer[..ApplePartitionMapEntry.Size]);
             if (partitionEntry.Type == "Apple_HFS")
             {
                 // Found the HFS partition - add a volume for it.
